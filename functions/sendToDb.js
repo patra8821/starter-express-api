fa596ce172508d52db220743ad3ad0e2abbe588e
Your Name <you@example.com>
2022-12-21 11:41:46 +0000

    log check

--- functions/sendToDb.js
+++ functions/sendToDb.js
@@ -2,11 +2,13 @@ const data = require("../model/data");
 
 const sendToDb = async (req, res) => {
   try {
+    console.log("called sendToDb");
     const dataData = new data({
       data: req.body.data,
     });
 
     await dataData.save();
+    console.log("success sendToDb");
     res.status(200).json({ message: "Success" });
   } catch (error) {
     console.log("Error On data savinig on db:", error.message);
